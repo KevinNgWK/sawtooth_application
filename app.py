42b7c0b4c9dbfe045a0dbb1d8cee658a07bcc770
Kevin <WNG052@e.ntu.edu.sg>
2021-12-13 14:54:52 +0800

    Finished Backend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import getpass
 import argparse
 import pkg_resources
 from flask import Flask, jsonify, request, abort
@@ -14,7 +15,6 @@ DISTRIBUTION_NAME = 'sawtooth-xo'
 
 AUTH_USER = None
 AUTH_PASSWORD = None
-# URL = 'tcp://172.19.174.169:4004'
 URL = 'http://localhost:8008'
 
 app = Flask(__name__)
@@ -28,7 +28,6 @@ def create_error_response(message, status_code=400):
 
 @app.route('/list')
 def list_games():
-
     client = XoClient(base_url=URL, keyfile=None)
 
     game_list = [
@@ -42,6 +41,90 @@ def list_games():
         return jsonify(game_list)
     else:
         raise XoException("Could not retrieve game listing.")
+
+
+@app.route('/show', methods=['POST'])
+def show_game():
+    name = request.get_json()['name']
+    client = XoClient(base_url=URL, keyfile=None)
+
+    data = client.show(name, auth_user=AUTH_USER, auth_password=AUTH_PASSWORD)
+
+    if data is not None:
+        board_str, game_state, player1, player2 = {
+            name: (board, state, player_1, player_2)
+            for name, board, state, player_1, player_2 in [
+                game.split(',')
+                for game in data.decode().split('|')
+            ]
+        }[name]
+        
+        board = list(board_str.replace("-", " "))
+        ret = {
+            'game': name,
+            'player 1': player1[:6],
+            'player 2': player2[:6],
+            'state': game_state,
+            'board': [board[:3], board[3:6], board[6:9]]
+        }
+        return jsonify(ret)
+    else:
+        raise XoException("Game not found: {}".format(name))
+
+
+@app.route('/create', methods=['POST'])
+def create_game():
+    req = request.get_json()
+    name, username = None, None
+    if 'name' in req:
+        name = req['name']
+    if 'username' in req:
+        username = req['username']
+    keyfile = _get_keyfile(username)
+    print(keyfile)
+
+    client = XoClient(base_url=URL, keyfile=keyfile)
+
+    response = client.create(name, auth_user=AUTH_USER, auth_password=AUTH_PASSWORD)
+    return jsonify(response)
+
+
+@app.route('/take', methods=['POST'])
+def take_position():
+    req = request.get_json()
+    name = req['name'] 
+    space = req['space']
+    username = req['username']
+
+    keyfile = _get_keyfile(username)
+
+    client = XoClient(base_url=URL, keyfile=keyfile)
+
+    response = client.take(name, space, auth_user=AUTH_USER, auth_password=AUTH_PASSWORD)
+    return jsonify(response)
+
+
+@app.route('/delete', methods=['POST'])
+def delete_game():
+    req = request.get_json()
+    name = req['name']
+    username = req['username']
+
+    keyfile = _get_keyfile(username)
+
+    client = XoClient(base_url=URL, keyfile=keyfile)
+
+    response = client.delete(name, auth_user=AUTH_USER, auth_password=AUTH_PASSWORD)
+    return jsonify(response)
+
+
+
+def _get_keyfile(username):
+    username = getpass.getuser() if username is None else username
+    home = os.path.expanduser("~")
+    key_dir = os.path.join(home, ".sawtooth", "keys")
+
+    return '{}/{}.priv'.format(key_dir, username)
 
 
 if __name__ == '__main__':
